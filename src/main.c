36911a44effb86f9e8e9f0d8d07c72de182bb907
kiryls <kiryl92@gmail.com>
2023-12-28 21:46:53 +0100

    movement on a grid

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
+#include <math.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #include "include/raylib.h"
 #include "include/raymath.h"
@@ -98,28 +97,32 @@ int main(int argc, char *argv[]) {
 
     char *window_title = "Ascii Wars";
     InitWindow(SCREEN_W, SCREEN_H, window_title);
-    int font_size = CELL_SIZE - 5;
+    int font_size = CELL_SIZE - 2;
     int font_space = 0;
-    Vector2 text_pos = {10, 10};
     float speed = 4;
     Font font = LoadFontEx("resources/FiraCodeNerdFont-Bold.ttf", font_size, 0, 255);
     Color friend = SKYBLUE;
     Color foe = ORANGE;
+    Vector2 text_pos = {5, 1};
 
-    Unit unit = {.type = U_INFANTRY, .role = I_ARCHER};
+    Unit unit = {.type = U_ARTILLERY, .role.artillery = A_SIEGE_TOWER};
 
     SetTargetFPS(60);
 
     Vector2 dir = {0};
+    Vector2 xy;
+    Vector2 fontCharSize;
     while (!WindowShouldClose()) { 
         if (IsKeyDown(KEY_D)) dir.x += 1.0f;
         if (IsKeyDown(KEY_A)) dir.x -= 1.0f;
         if (IsKeyDown(KEY_W)) dir.y -= 1.0f;
         if (IsKeyDown(KEY_S)) dir.y += 1.0f;
 
+        // update text_pos
+
         dir = Vector2Normalize(dir);
 
-        text_pos = Vector2Add(text_pos, Vector2Scale(dir, speed));
+        text_pos = Vector2Add(text_pos, Vector2Scale(dir, CELL_SIZE));
 
         dir = Vector2Zero();
 
@@ -127,8 +130,10 @@ int main(int argc, char *argv[]) {
         ClearBackground(COLORS(COLOR_BG));
         
         DrawCells();
-        
-        DrawTextEx(font, GetAvatar(unit), text_pos, font_size, font_space, COLORS(COLOR_TEXT));
+        fontCharSize = MeasureTextEx(font, "@", font_size, font_space);
+        xy.x = floor(text_pos.x/CELL_SIZE) * CELL_SIZE + (CELL_SIZE - fontCharSize.x)/2; 
+        xy.y = floor(text_pos.y/CELL_SIZE) * CELL_SIZE + (CELL_SIZE - fontCharSize.y)/2; 
+        DrawTextEx(font, GetAvatar(unit), xy, font_size, font_space, COLORS(COLOR_TEXT));
         EndDrawing();
     }
 
